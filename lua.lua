2299169ce665d449fec37a52f120789d80e1fbba
FUNNYSADCAT <thanap198@gmail.com>
2026-01-09 11:33:04 +0700

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,5 +1,5 @@
 local Games = {
-    [128001665358186] = ""
+    [128001665358186] = "https://raw.githubusercontent.com/FUNNYSADCAT/Null-hub-shawarma/refs/heads/main/Shawarma.lua" -- Shawarma
 }
 
 return Games
